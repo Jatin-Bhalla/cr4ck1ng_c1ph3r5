56fcd3546186f41dd0fbbd443dbb1943cdb96d94
PI <jatin241001@gmail.com>
2022-08-04 12:34:17 +0530

    Update Caesarbruteforce.py
    this one works

--- Caesarbruteforce.py
+++ Caesarbruteforce.py
@@ -1,17 +1,21 @@
 # WE brute fore the caesar cipher with python script
-cipher =input("enter the cipher:\n -->")
-symbol='abcdefghijklmnopqrstuvwxyz'
+""" This code ends up providing with every possiblility for the cipher for the english language as there are only 26 letters therefore only 25 possible crypts exist"""
+import string
+import collections
 
-"""we need to use loop to end up with every possible outcome which may or may not make any sense"""
-for k in range(len(symbol)):
-    plain=""
-    for symbol in message:
-        if symbol in symbol:
-            symbolIndex=symbol.find(symbol)
-            translatedIndex =symbolIndex -k
-            if  translatedIndex<0:
-                translatedIndex = translatedIndex +len(symbol)
-                translated = translated +symbol[translatedIndex]
-            else:
-                 translated =translated+symbol
-            print('kay # %s:%s'%(k,translated))
+
+# this code pieces uses rotate for caesar cipher like literally rorate LOL
+def Caesar(input ,k):
+    upper=collections.deque(string.ascii_uppercase)
+    lower =collections.deque(string.ascii_lowercase)
+
+    upper.rotate(k)
+    lower.rotate(k)
+
+    upper ='' .join(list(upper))
+    lower ='' .join(list(lower))
+    return input.translate(str.maketrans(string.ascii_uppercase,upper)).translate(str.maketrans(string.ascii_lowercase,lower))
+
+plain =input("ENTER YOU PLAIN TEXT:")
+for i in range(len(string.ascii_uppercase)):
+    print( i,"|" , Caesar(plain,1))
