a73391caceb8d81f6c836bfc8577e7661ed3e39e
PI <jatin241001@gmail.com>
2022-08-04 12:38:09 +0530

    Update Caesarbruteforce.py
    I also added the code that wasn't working in case I have a EUREKA moment

--- Caesarbruteforce.py
+++ Caesarbruteforce.py
@@ -19,3 +19,41 @@ def Caesar(input ,k):
 plain =input("ENTER YOU PLAIN TEXT:")
 for i in range(len(string.ascii_uppercase)):
     print( i,"|" , Caesar(plain,1))
+    
+"""I have this another piece of code below I saw online using ord()"""
+#check for errors in this line :43
+"""
+import string
+# first ask if you want to encrypt or decrypt
+whatto_do =input("Encrypt=1 \t Bruteforce=0 \t choose what you want to proceed with :\n -->")
+plain=input("Enter plain text:\n")
+#now if ypu chose encrypt
+if whatto_do =="1":
+    k=int(input("Enter your key(0-26):\t"))
+
+#now the basic stuff is done but we need to  define  a function  to solve the caesar cipher and we need to provide it with what all it does
+def Caesar(input,k):
+    #k is a global variable whose value will be used
+    output=""
+    input=input.lower()
+    #define a input and output cuz you will get a plain text and a encrypted text although there is already variabales for that so u define new local variables instead of the local variable
+    for x in input:
+        if x in str.ascii_letters:
+            temp = ord(x)+k
+            if temp > ord("z")             # this line is showing syntax error!?
+            temp=temp-26
+            #this is done cuz if the letter exceeds z it goes back to a
+            output =output+chr(temp)
+        else:
+            output=output+x
+            return output
+#now that the cipher is complete
+if  whatto_do =="1":
+    print(Caesar(plain,k))
+else:
+    #this code is what will work for brute force
+    for  k in range(26):
+        print("key = "+str(k))
+        print(Caesar(plain,k))
+
+"""
