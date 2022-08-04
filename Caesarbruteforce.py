3d56d1259bf8c4888e8affebf13879a8b2c53716
PI <jatin241001@gmail.com>
2022-08-04 11:10:08 +0530

    Update Caesarbruteforce.py
    trial#1

--- Caesarbruteforce.py
+++ Caesarbruteforce.py
@@ -1,1 +1,17 @@
 # WE brute fore the caesar cipher with python script
+cipher =input("enter the cipher:\n -->")
+symbol='abcdefghijklmnopqrstuvwxyz'
+
+"""we need to use loop to end up with every possible outcome which may or may not make any sense"""
+for k in range(len(symbol)):
+    plain=""
+    for symbol in message:
+        if symbol in symbol:
+            symbolIndex=symbol.find(symbol)
+            translatedIndex =symbolIndex -k
+            if  translatedIndex<0:
+                translatedIndex = translatedIndex +len(symbol)
+                translated = translated +symbol[translatedIndex]
+            else:
+                 translated =translated+symbol
+            print('kay # %s:%s'%(k,translated))
