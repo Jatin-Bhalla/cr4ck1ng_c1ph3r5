6918e43bb33caa2643852ac05b3b143ac47c570b
PI <jatin241001@gmail.com>
2022-08-06 12:13:04 +0530

    Update Caesarbruteforce.py
    this seemingly works fine

--- Caesarbruteforce.py
+++ Caesarbruteforce.py
@@ -2,23 +2,18 @@
 """ This code ends up providing with every possiblility for the cipher for the english language as there are only 26 letters therefore only 25 possible crypts exist"""
 import string
 import collections
-
-
 # this code pieces uses rotate for caesar cipher like literally rorate LOL
 def Caesar(input ,k):
     upper=collections.deque(string.ascii_uppercase)
     lower =collections.deque(string.ascii_lowercase)
-
     upper.rotate(k)
     lower.rotate(k)
-
-   upper ='' .join(list(upper))
-   lower ='' .join(list(lower))
+    upper ='' .join(list(upper))
+    lower ='' .join(list(lower))
     return input.translate(str.maketrans(string.ascii_uppercase,upper)).translate(str.maketrans(string.ascii_lowercase,lower))
-
 plain =input("ENTER YOU PLAIN TEXT:")
 for i in range(len(string.ascii_uppercase)):
-    print( i,"\t" , Caesar(plain,1))
+    print( i,"|" , Caesar(plain,1))
     
 """I have this another piece of code below I saw online using ord()"""
 #check for errors in this line :43
